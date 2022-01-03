d16e952b2a386a2e3a68872940ec2b5fbe7e0208
Documenter.jl <documenter@juliadocs.github.io>
2022-01-03 20:55:42 +0000

    build based on 7ae84d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LoopPoly","category":"page"},{"location":"#LoopPoly","page":"Home","title":"LoopPoly","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LoopPoly.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LoopPoly]","category":"page"}]
+}
